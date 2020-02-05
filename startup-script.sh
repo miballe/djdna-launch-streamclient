b9c91818132486bd401ea23ccca65e347bf35183
Miguel Ballesteros <miballegh@outlook.com>
2020-02-05 18:03:29 +0100

    Adapted to use the factiva client GitHub

--- startup-script.sh
+++ startup-script.sh
@@ -4,14 +4,12 @@ apt-get install -y git
 apt-get install -y python-pip
 apt-get install -y virtualenv
 apt-get update
-git clone https://github.com/miballe/dj-dna-streams-python.git /opt/dna-stream-client
-virtualenv -p /usr/bin/python3 /opt/dna-stream-client/venv
-source /opt/dna-stream-client/venv/bin/activate
-pip install -r /opt/dna-stream-client/requirements.txt
-python /opt/dna-stream-client/setup.py install
-mkdir /opt/dna-stream-client/dnaStreaming/articles
+git clone https://github.com/dowjones/factiva-streams-python.git /opt/factiva-stream-client
+virtualenv -p /usr/bin/python3 /opt/factiva-stream-client/venv
+source /opt/factiva-stream-client/venv/bin/activate
+pip install -r /opt/factiva-stream-client/requirements.txt
 export SUBSCRIPTION_ID="SUBSCRIPTION_ID"
 export USER_KEY="USER_KEY"
-cd /opt/dna-stream-client
-python /opt/dna-stream-client/setup.py install
-python ./dnaStreaming/demo/show_stream.py -s
+export GCP_PROJECT_ID="GCP_PROJECT_ID"
+export GCP_PUBSUB_TOPIC="GCP_PUBSUB_TOPIC"
+python /opt/factiva-stream-client/fs_to_gcp_pubsub.py
